de8356eedec4f9ffb9c848d3b633217723e10a09
alicefurquimsouza-ui <alice.furquim.souza@escola.pr.gov.br>
2026-08-21 10:47:31 -0300

    ok

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
      document.addEventListener ("DOMContentLoadeo",()=> {
+      // Seliciona p botão de curtida e o ícone de coração 
      const LikeBtn = document.querySelector(".left-actions.action-btn: first-child");
-     if (!likeBtn)
+     if (!likeBtn) return;
+
+     const likeSvg = likeBtn.querySelector ("svg");
+
+     // contador 
+     let textNode = Array.from(likeBtn.childNodes).find(
+      (node) => node.nodeType === Node. TEXT_NODE && node.textContent.trim()!== ""
+     );
+
+     // Zerando o contador inicial.
+     let count = 0;
+
+     // Atualiza
+     if (textNode) {
+      textNode.textContent = ` 0`;
+     }
         })
